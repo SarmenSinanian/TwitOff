8527bd55399006d1d2f8b2df21fa7df5540e94f5
SarmenSinanian <sarmen.sinanian@yahoo.com>
2020-04-03 16:28:31 -0700

    Render JSON Data

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 def create_app():
     """Create and configure and instance of the Flask application."""
@@ -7,5 +7,20 @@ def create_app():
     @app.route('/')
     def root():
         return 'Hello TwitOff!'
+
+    @app.route('/about')
+    def about():
+        return 'This is an app to guess who is more likely to say a ' \
+               'user-created tweet between two existing twitter users' \
+               ' by using their existing tweet history!'
+
+    @app.route('/books')
+    @app.route('/books.json')
+    def list_books():
+        books = [
+            {'id': 1, 'title': 'book1'},
+            {'id': 2, 'title': 'book2'}
+        ]
+        return jsonify(books)
 
     return app
